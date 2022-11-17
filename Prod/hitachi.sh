739d35fed15d150ea5e67c330c2dd6b1047584ba
YogeshBaghel09 <115460005+YogeshBaghel09@users.noreply.github.com>
2022-11-17 15:51:27 +0530

    Update hitachi.sh

--- Prod/hitachi.sh
+++ Prod/hitachi.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
               sudo su
-              #sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-              sudo yum update -y
-              sudo yum install httpd -y
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-              sudo echo 'Hello World This is prod' >> /var/www/html/index.html
-              sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-              sudo sed -i 's/Listen 80/Listen 8080/g' /etc/httpd/conf/httpd.conf
-              sudo systemctl restart httpd
-              sudo systemctl restart sshd
+              sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+              yum update -y
+              yum install httpd -y
+              systemctl start httpd
+              systemctl enable httpd
+              echo 'Hello World This is prod' >> /var/www/html/index.html
+              sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+              sed -i 's/Listen 80/Listen 8080/g' /etc/httpd/conf/httpd.conf
+              systemctl restart httpd
+              systemctl restart sshd
+              curl localhost:8080
               
