e2688fff0038cc775706c41b07127483623bf8e2
YogeshBaghel09 <115460005+YogeshBaghel09@users.noreply.github.com>
2022-11-17 12:52:22 +0530

    Update hitachi.sh

--- DEV/hitachi.sh
+++ DEV/hitachi.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-              sudo su
-              #yum update -y
-              sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-              yum install httpd -y
-              systemctl start httpd
-              systemctl enable httpd
-              echo 'Hello World this is DEV' >> /var/www/html/index.html
-              sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+              #sudo su
+              sudo yum update -y
+              #sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+              sudo yum install httpd -y
+              sudo systemctl start httpd
+              sudo systemctl enable httpd
+              sudo echo 'Hello World this is DEV' >> /var/www/html/index.html
+              sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
               #sed -i 's/Listen 80/Listen 8080/g' /etc/httpd/conf/httpd.conf
-              systemctl restart httpd
-              systemctl restart sshd
+              sudo systemctl restart httpd
+              sudo systemctl restart sshd
