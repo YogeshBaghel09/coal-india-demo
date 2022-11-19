9b1db94273070384b982c1722096925d1bee1ffc
YogeshBaghel09 <115460005+YogeshBaghel09@users.noreply.github.com>
2022-11-19 15:28:43 +0530

    Update backend.tf

--- DEV/backend.tf
+++ DEV/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "coal-india-demo-s3-bucket"
     #dynamodb_table = "s3-terraform-eks-state-lock"
-    key     = "terraform.tfstate"
+    key     = "dev/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
   }
